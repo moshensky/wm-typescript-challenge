29086d2ef780aa86e15a0f2a68a92afb5c538295
Nikita Moshensky <nikita@moshensky.com>
2022-07-08 07:08:12 +0300

    dry Navbar a bit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
 import { ROUTES } from "router/routes";
+import { HamburgerButton } from "components/hamburger-button";
 import { Logo } from "../logo";
 import { ColorThemeToggle } from "../color-theme-toggle/ColorThemeToggle";
 
@@ -9,6 +10,16 @@ const navbarStyles = {
   active:
     "block rounded bg-blue-700 py-2 pr-4 pl-3 text-white dark:text-white md:bg-transparent md:p-0 md:text-gray-900 underline dark:md:text-white",
 };
+
+type MenuItem = {
+  testId: string;
+  route: string;
+  label: string;
+};
+const menuItems: ReadonlyArray<MenuItem> = [
+  { testId: "navbar-link--home", route: ROUTES.HOME, label: "Home" },
+  { testId: "navbar-link--recipes", route: ROUTES.RECIPES, label: "Recipes" },
+];
 
 export const Navbar = () => {
   return (
@@ -20,64 +31,22 @@ export const Navbar = () => {
         </Link>
 
         <ColorThemeToggle className="mr-6" />
-
-        <button
-          data-collapse-toggle="mobile-menu"
-          type="button"
-          className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-          aria-controls="mobile-menu"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="h-6 w-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <svg
-            className="hidden h-6 w-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
+        <HamburgerButton className="md:hidden" />
         <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
           <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
-            <li>
-              <NavLink
-                data-testid="navbar-link--home"
-                to={ROUTES.HOME}
-                className={({ isActive }) =>
-                  isActive ? navbarStyles.active : navbarStyles.default
-                }
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                data-testid="navbar-link--recipes"
-                to={ROUTES.RECIPES}
-                className={({ isActive }) =>
-                  isActive ? navbarStyles.active : navbarStyles.default
-                }
-              >
-                Recipes
-              </NavLink>
-            </li>
+            {menuItems.map((x) => (
+              <li key={x.testId}>
+                <NavLink
+                  data-testid={x.testId}
+                  to={x.route}
+                  className={({ isActive }) =>
+                    isActive ? navbarStyles.active : navbarStyles.default
+                  }
+                >
+                  {x.label}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
